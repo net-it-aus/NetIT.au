b523c7f5dec4967e46ccc52f1ece51784dbab082
donald garton <support@netit.com.au>
2024-07-26 11:13:04 +1000

    initial-commit

--- js/!main.js
+++
@@ -1,8 +0,0 @@
-// collapse all    Ctrl + k + 0
-// expand all      Ctrl + k + j
-
-function goTop() {
-    document.body.scrollTop = 0; // For Safari
-    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-}
-
